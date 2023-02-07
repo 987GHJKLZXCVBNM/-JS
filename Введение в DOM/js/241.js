84eb7fe36e4481a9c9b43cac7c6518cc55688659
Retro6790 <114397005+987GHJKLZXCVBNM@users.noreply.github.com>
2023-02-07 17:48:00 +0500

    Add files via upload

---
+++ Введение в DOM/js/241.js
@@ -0,0 +1,26 @@
+'use strict'
+// -----------------------------------------1
+ /*let elem1 = document.querySelector('#elem1');
+  console.log(elem1);
+  let elem2 = document.querySelector('#elem2');
+ console.log(elem2);
+ let elem3 = document.querySelector('#elem3');
+  console.log(elem3);
+
+
+// -----------------------------------------2
+
+ let block1 = document.querySelector('#block1 p');
+ console.log(block1);
+
+
+// ----------------------------------------3
+
+ let block2 = document.querySelector('.block2 p');
+   console.log(block2);
+
+// --------------------------4
+
+ let www = document.querySelector('.www');
+ console.log(www);
+ */
